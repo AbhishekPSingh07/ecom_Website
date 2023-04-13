c25b16156ef249335e2dd25f679444a1293d6368
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-04-13 23:12:57 +0530

    Intial creation of repo

---
+++ src/Components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Navbar.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const Navbar = () => {
+  return (
+    <div className='Container'>
+      <div className= "wrapper">
+        <div className='left'>
+          <div className='language'>EN</div>
+          <div className='search-container'>
+            <input className='search-input'></input>
+            <SearchIcon/>
+          </div>
+        </div>
+        <div className='centre'>
+          <h1 className='logo'>ABHI.</h1>
+          
+        </div>
+        <div className='right'>
+          <div className='nav_menu_item'>Register</div>
+          <div className='nav_menu_item'>Sign In</div>
+          <div className='nav_menu_item'>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartIcon color="action" />
+            </Badge>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
