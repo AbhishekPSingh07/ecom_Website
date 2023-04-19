43480380061a00285aa8f5d22a9a5458a8d0838a
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-04-19 09:54:59 +0530

    added Slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import './Slider.scss'
+import { sliderItems } from '../Data';
 const Slider = () => {
+    const [slideIndex,setSlideIndex] = useState(0);
+    const handleClick = (direction)=>{
+        if(direction === 'left'){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1: 1)
+        }else{
+            setSlideIndex(slideIndex< 1 ? slideIndex+1: 0)
+        }
+    };
   return (
     <div className='container-slider'>
-      <div className='Arrow left'>
+      <div className='Arrow left' onClick={()=>handleClick('left')}>
         <ArrowBackIosIcon/>
       </div>
-      <div className='wrapper-slider'>
-        <div className='slide'>
+      <div className='wrapper-slider' style={{transform: `translateX(${slideIndex * -100}vw)`}}>
+        {sliderItems.map((item)=>(
+        <div className='slide' style={{backgroundColor: item.bg}}>
             <div className='image-container'>
-                <img className='image' src='https://i.ibb.co/XsdmR2c/1.png' alt=''/>
+                <img className='image' src={item.img} alt=''/>
             </div>
-            <div className='onfo-container'></div>
+            <div className='info-container'>
+                <h1 className='title'>{item.title}</h1>
+                <p className='description'>{item.desc}</p>
+                <button className='button'>Shop Now</button>
+            </div>
         </div>
+        ))}
       </div>
-      <div className='Arrow right'>
+      <div className='Arrow right' onClick={()=>handleClick('right')}>
         <ArrowForwardIosIcon/>
       </div>
     </div>
